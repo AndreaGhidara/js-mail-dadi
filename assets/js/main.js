d8b9503b53f41db73d015ad66601275d9a4e137a
Andrea Ghidara <Ghidara.andrea.99@gmail.com>
2023-04-18 17:27:48 +0200

    beta

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ const btn = document.querySelector(".btn");
 const textInput = document.querySelector(".input");
 const txt = document.querySelector(".txt");
 
+let log = false;
 
 const userLogged = ["andrea@gmail.com", "luca@gmail.com", "giorgio@gmail.com", "marco@gmail.com", "kilua125@gmail.com"];
 
@@ -10,14 +11,22 @@ btn.addEventListener("click", function() {
     const email = document.querySelector("input").value;
     for (let i = 0; i < userLogged.length; i++) {
         const actualUser = userLogged[i];
+        console.log(actualUser);
         
         if ( actualUser == email ) {
-            alert("bentornato");
-            break;
-        } else {
-            txt.innerHTML = "Hai sbagliato qualcosa, riprova";
-            let actualValue = document.querySelector("input").value
+            log = true;
         }
         
+
+
+        if ( log == true ) {
+            let testo = document.querySelector("h2");
+            testo.innerText = "si";
+
+        } else {
+            let testo = document.querySelector("h2");
+            testo.innerText = "no";
+
+        }
     }
 })
